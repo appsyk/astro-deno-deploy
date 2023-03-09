8534bbe08e8604019ade168e227ff9a904fe9e72
sanjay.khatal <sanjay.khatal@ayanworks.com>
2023-03-09 16:28:59 +0530

    updated path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
-
 // https://astro.build/config
-import deno from "@astrojs/deno";
+import deno from "/@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
